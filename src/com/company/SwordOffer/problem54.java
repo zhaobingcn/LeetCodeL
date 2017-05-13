e08c40c8b7d00c74f4181ab85a310b0c5edc7a4b
zhaobingcn <zhaobingcn@hotmail.com>
2017-05-13 21:50:55 +0800

    5.13

---
+++ src/com/company/SwordOffer/problem54.java
@@ -0,0 +1,32 @@
+package com.company.SwordOffer;
+
+/**
+ * Created by zhzy on 2017/5/13.
+ */
+public class problem54 {
+
+    public static void main(String[] args){
+
+        char[] a = {'q', 'b'};
+        for(int i=0; a[i] != '\0'; i++){
+            System.out.println(a[i]);
+        }
+    }
+
+//    public static boolean match(char[] str, char[] pattern)
+//    {
+//
+//    }
+//
+//    public static boolean matchCore(char[] str, char[] pattern){
+//
+//        if(str.length ==0 && pattern.length ==0){
+//            return true;
+//        }
+//        if(str.length >0 && pattern.length ==0){
+//            return false;
+//        }
+//
+//
+//    }
+}
--- src/com/company/SwordOffer/problem_45.java
+++ src/com/company/SwordOffer/problem_45.java
@@ -1,7 +1,27 @@
 package com.company.SwordOffer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zhzy on 2017/5/13.
  */
 public class problem_45 {
+
+    public static void main(String[] args){
+
+    }
+    public int LastRemaining_Solution(int n, int m) {
+        List<Integer> circle = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            circle.add(i);
+        }
+
+        int curIndex = m-1;
+        while(circle.size() > 1){
+            circle.remove(curIndex);
+            curIndex = (curIndex + m) % circle.size();
+        }
+        return circle.get(0);
+    }
 }
