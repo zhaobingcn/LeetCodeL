3fbba9d6ba27bb91db4c199e6e4d97f12ba179ab
zhaobingcn <zhaobingcn@hotmail.com>
2017-10-03 16:09:43 +0800

    增加了新的算法

---
+++ src/com/Algorthms/Depth_first_search.java
@@ -0,0 +1,59 @@
+package com.Algorthms;
+
+import java.util.*;
+
+/**
+ * Created by zhzy on 2017/10/2.
+ */
+public class Depth_first_search {
+
+    public static void main(String[] args) {
+        Employee a = new Employee(1,2, Arrays.asList(2));
+        Employee b = new Employee(2,3, Arrays.asList());
+
+        List<Employee> c = new ArrayList<>();
+        c.add(a);
+        c.add(b);
+        System.out.println(getImportance(c, 2));
+
+    }
+
+    public static int getImportance(List<Employee> employees, int id) {
+
+        HashMap<Integer, Employee> map = new HashMap<>();
+        for(Employee e: employees){
+            map.put(e.id, e);
+        }
+
+        Queue<Employee> queue = new LinkedList<>();
+        queue.add(map.get(id));
+        int result = 0;
+        while(!queue.isEmpty()){
+            Employee e = queue.poll();
+            result += e.importance;
+            if(e.subordinates.size() != 0){
+                List<Integer> list = e.subordinates;
+                for(Integer i: list){
+                    Employee ae = map.get(i);
+                    queue.add(ae);
+                }
+            }
+        }
+        return result;
+    }
+     static class Employee {
+        // It's the unique id of each node;
+        // unique id of this employee
+        public int id;
+        // the importance value of this employee
+        public int importance;
+        // the id of direct subordinates
+        public List<Integer> subordinates;
+
+        public Employee(int id, int importance, List<Integer> subordinates){
+            this.id = id;
+            this.importance = importance;
+            this.subordinates = subordinates;
+        }
+    }
+}
--- src/com/Algorthms/dynamicprograming/Maximum_Product_Subarray.java
+++ src/com/Algorthms/dynamicprograming/Maximum_Product_Subarray.java
@@ -6,4 +6,72 @@ package com.Algorthms.dynamicprograming;
 public class Maximum_Product_Subarray {
 
 
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{3,-1,4};
+        System.out.println(maxProduct(nums));
+
+    }
+    public static int maxProduct(int[] nums) {
+        int r = nums[0];
+
+        return 0;
+    }
+
+    /**
+     * 错误的解法
+     */
+
+//    public int maxProduct(int[] nums) {
+//
+//        int len = nums.length;
+//        int dp[][] = new int[2][len + 1];
+//
+//        // for(int i=0; i<2; i++){
+//        //     for(int j=0; j<=len; j++){
+//        //         dp[i][j] = 1;
+//        //     }
+//        // }
+//        dp[0][1] = nums[0];
+//        dp[1][1] = nums[0];
+//
+//        int max = Integer.MIN_VALUE;
+//        int tag = -10;
+//
+//        for(int i=2; i<len; i++){
+//            if(nums[i] > 0){
+//                dp[0][i+1] = dp[0][i] * nums[i];
+//                if(dp[1][i] < 0){
+//                    dp[1][i+1] = dp[1][i] * nums[i];
+//                }
+//                if(dp[0][i+1] > max){
+//                    max = dp[0][i+1];
+//                    if(max == 1){
+//                        tag = i;
+//                    }
+//                }
+//            }else if(nums[i] == 0){
+//                dp[0][i+1] = 1;
+//                dp[1][i+1] = 1;
+//                if(0 > max){
+//                    max = 0;
+//                }
+//            }else{
+//                if(dp[1][i] < 0){
+//                    dp[0][i+1] = dp[1][i] * nums[i];
+//                }
+//                dp[1][i+1] = dp[0][i] * nums[i];
+//                if(dp[0][i+1] > max){
+//                    max = dp[0][i+1];
+//                    if(max == 1){
+//                        tag = i;
+//                    }
+//                }
+//            }
+//        }
+//        if(max == 1 && nums[0] != -1){
+//            max = nums[tag];
+//        }
+//        return max;
+//    }
 }
